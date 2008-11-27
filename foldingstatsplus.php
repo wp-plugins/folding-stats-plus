2ce73de4ba407c8a180b3288c010f824297d389d
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-27 20:24:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/folding-stats-plus/tags/0.9.3@76174 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ foldingstatsplus.php
@@ -0,0 +1,177 @@
+<?php
+/*
+Plugin Name: Folding Stats Plus
+Plugin URI: http://www.pross.org.uk/category/plugins/
+Description: This plugin is intended to show the current Folding@Home statistics for a given account. <a href="options-general.php?page=folding-stats-plus/options-folding.php">Settings</a> page.
+Version: 0.9.1
+Author: Simon Prosser
+Author URI: http://www.pross.org.uk
+Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+*/
+/*	Code is forked with permission from Jason F. Irwin J�fi's version http://www.j2fi.net/2007/03/23/foldinghome-wordpress-plugin/
+	
+	Copyright 2007  Simon Prosser  (email: pross@pross.org.uk)
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+function fold_init() {
+  	define('FOLD_ACCT', get_option('folding_acct'));
+  	define('FOLD_EXPY', get_option('folding_expy'));
+	}
+add_action('init','fold_init');
+function folding() {
+get_folding_stats();
+}
+function widget_folding($args) {
+	extract($args);
+$title = get_option('widget_folding_title');
+echo $before_widget;
+echo $before_title . $title . $after_title;
+folding();
+	echo $after_widget;
+}
+	// Settings form
+	function widget_folding_control() {
+ 		// Get options
+		$options = get_option('widget_folding_title');
+		// options exist? if not set defaults
+		if ( !$options ) {
+			$options = 'Folding Stats';
+add_option('widget_folding_title',$options);
+}
+                      // form posted?
+		if ( $_POST['folding-submit'] ) {
+ 			// Remember to sanitize and format use input appropriately.
+			$options = strip_tags(stripslashes($_POST['folding-title']));
+			update_option('widget_folding_title', $options);
+		}
+		// Get options for form fields to show
+		$title = htmlspecialchars($options, ENT_QUOTES);
+		// The form fields
+		echo '<p style="text-align:right;">
+				<label for="folding-title">' . __('Title:') . '
+				<input style="width: 200px;" id="folding-title" name="folding-title" type="text" value="'.$title.'" />
+				</label></p>';
+		echo '<input type="hidden" id="folding-submit" name="folding-submit" value="1" />';
+		}
+function folding_init() {
+  register_sidebar_widget(__('Foldingstats'), 'widget_folding');
+  register_widget_control(array('Foldingstats','widgets'), 'widget_folding_control');
+}
+add_action("plugins_loaded", "folding_init");
+function get_folding_stats() {
+	if (get_option('folding_acct') != 'fold-id') {
+	//Date Compare
+	$today = mktime(date("H"), 0, 0, date("m"), date("d"), date("y"));
+	$expiry = get_option('folding_expire');
+if (!$expiry) { 
+	$expiry = 0;
+			}
+		if ($expiry < $today) {
+			//Get Fresh Data
+			read_fold_site();
+			}
+	//Read Data From Cache
+	$fold_logo = get_settings('home') . '/wp-content/plugins/folding-stats-plus/FAHlogoML.jpg';
+	//Output
+	$out = '<div align="'.get_option('folding_align').'"><p>';
+	$out = $out .'Total Score: <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_credit').'</font><br />';
+	$out = $out .'OverallRank: <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_rank').'</font><br />';
+	$out = $out .'WorkUnits  : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_wu').'</font><br />';
+	if (get_option('folding_wut')) {
+	$out = $out .'OtherUnits : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_wut').'</font><br />';
+	}
+	$out = $out .'LastUpdate : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_last').'</font>';
+	if (get_option('folding_pic') == 'true') {
+	$out = $out . '<br /><a href="http://folding.stanford.edu"><img style="border:none;" src="'.$fold_logo.'" alt="Folding@Home" /></a>';
+	}
+	$out = $out . '</p></div>';
+	echo $out;
+	echo '
+<!-- This is a HTML comment it will not appear in your post page!
+Folding-stats-plus http://www.pross.org.uk/wordpress-plugins/ 
+Feel free to leave a comment suggestion -->';
+	} else {
+		echo 'Check settings!';
+	}
+	}
+function read_fold_site() {
+if (get_option('folding_acct') != 'fold-id') {
+	$host = 'fah-web.stanford.edu';
+	$path = '/cgi-bin/main.py?qtype=userpage&username=' . FOLD_ACCT;
+	$stats_url = 'http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username=' . FOLD_ACCT;
+	$fold_url = 'http://folding.stanford.edu/';
+	//Get the site data and trim to something managable
+	$sFile = get_contents($stats_url, False);
+	$sfile = substr($sfile, 0, 2000);
+	$last_upd = strstr($sFile, 'Date of last work unit');
+	$credit = strstr($sFile, 'Total score');
+	$ov_rank = strstr($sFile, 'Overall rank');
+	$wu = strstr($sFile, '<TD> WU</TD>');
+	$wu2 = strstr($wu, '<TD align=left><b>Active');
+	$wu2 = strstr($wu2, '<TD> WU</TD>');
+	//Parse Strings
+	$last_upd = substr($last_upd, strpos($last_upd, '=4>') + 4, 11);
+	$credit = substr($credit, strpos($credit, '=4>') + 4, 12);
+	$credit = substr($credit, 0, strpos($credit, '<'));
+	$ov_rank = substr($ov_rank, strpos($ov_rank, '=4>') + 4, 20);
+	if (get_option('folding_rank_show') == 'short') {
+		$ov_rank = substr($ov_rank, 0, strpos($ov_rank, 'o')); 
+		} else {
+				$ov_rank = substr($ov_rank, 0, strpos($ov_rank, '<'));
+		}
+	$wu = substr($wu, strpos($wu, '<b>') + 4, 4);
+	$wu = substr($wu, 0, strpos($wu, '<'));
+	$wu2 = substr($wu2, strpos($wu2, '<b>') + 4, 4);
+	$wu2 = substr($wu2, 0, strpos($wu2, '<'));
+	$expire = mktime(date("H")+FOLD_EXPY, 0, 0, date("m"), date("d"), date("y"));
+	//Write to Cache
+	if ($updating = strstr($sFile, 'Stats update in progress')) { 
+		$expire = mktime(date("H")+1, 0, 0, date("m"), date("d"), date("y"));;
+		update_option('folding_expire',$expire);
+	} else {
+		update_option('folding_expire',$expire);
+	update_option('folding_credit',$credit);
+	update_option('folding_rank',$ov_rank);
+	update_option('folding_wu',$wu);
+	update_option('folding_wut',$wu2);
+	update_option('folding_last',$last_upd);
+	}
+}
+} 
+/** filters**/
+add_action('admin_head', 'fold_add_options_page');
+function fold_add_options_page() {
+	add_options_page('Folding Options', 'Folding Options', 'manage_options', 'folding-stats-plus/options-folding.php');
+	}
+function get_contents($url) {
+	if(ini_get('allow_url_fopen'))
+		{
+		return file_get_contents($url);
+		}
+	else if(function_exists('curl_init')) {
+			$ch = curl_init();
+			$timeout = 5; // set to zero for no timeout
+			curl_setopt ($ch, CURLOPT_URL, $url);
+			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+			$file_contents = curl_exec($ch);
+			curl_close($ch);
+			return $file_contents;
+			} else {
+				echo 'This plugin requires you to have either allow_url_fopen or cURL. Please enable allow_url_fopen or install cURL to continue.';
+			}
+		}
+?>
---
+++ options-folding.php
@@ -0,0 +1,116 @@
+<?php
+/*
+	Author:  Simon Prosser
+	Author URI: http://www.pross.org.uk
+	Description: Displays Folding@Home stats
+*/
+load_plugin_textdomain('foldhome');
+/* Create Default Account Number if it Doesn't Exist */
+add_option('folding_acct', 'fold-id');
+add_option('folding_expy', '4');
+add_option('folding_align', 'left');
+add_option('folding_pic', 'true');
+add_option('folding_results_bold', 'true');
+add_option('folding_results_color', '000000');
+add_option('folding_rank_show', 'short');
+$location = get_option('foldurl') . '/wp-admin/admin.php?page=folding-stats-plus/options-folding.php'; // Form Action URI
+$script_directory = substr(__FILE__, 0, strrpos(__FILE__, '/'));
+$foldfile = $script_directory . '/folding_cache.txt';
+/*check form submission and update options*/
+if ('process' == $_POST['stage'])
+{
+if ($_POST['fold_acct'] == '') { 
+	update_option('folding_acct', 'fold-id');
+	} else { 
+	update_option('folding_acct', $_POST['fold_acct']);
+	}
+  update_option('folding_expy', $_POST['fold_expy']);
+  update_option('folding_align', $_POST['fold_align']);
+  update_option('folding_results_color', $_POST['fold_results_color']);
+		if (isset($_POST['fold_pic'])) {
+			update_option('folding_pic', 'true');
+		} else {
+			update_option('folding_pic', 'false');
+		}
+		if (isset($_POST['fold_results_bold'])) {
+			update_option('folding_results_bold', 'bold');
+		} else {
+			update_option('folding_results_bold', 'normal');
+		}
+			if (isset($_POST['fold_rank'])) {
+			update_option('folding_rank_show', 'full');
+			update_option('folding_expire',0);
+			read_fold_site();
+		} else {
+			update_option('folding_rank_show', 'short');
+			update_option('folding_expire',0);
+			read_fold_site();
+		}	
+if (isset($_POST['fold_del'])) {
+update_option('folding_expire',0);
+read_fold_site();
+}
+}
+/*Get options for form fields*/
+$fold_acct = get_option('folding_acct');
+$fold_expy = get_option('folding_expy');
+$fold_align = get_option('folding_align');
+$fold_pic = get_option('folding_pic');
+$fold_results_bold = get_option('folding_results_bold');
+$fold_results_color = get_option('folding_results_color');
+$fold_rank = get_option('folding_rank_show');
+?>
+<form name="fold"></form>
+<div class="wrap">
+  <h2><?php _e('Folding Options') ?></h2>
+<br class="clear" />
+  <form id="fold_form" name="form1" method="post" action="<?php echo $location ?>&amp;updated=true">
+  	<input type="hidden" name="stage" value="process" />
+  	<fieldset class="options">
+		<div id="poststuff" class="ui-sortable">
+		<div id="folding-stats_settings" class="postbox " >	
+  		<h3><?php _e('Personal Settings') ?></h3>
+<div class="inside">
+  		<p><label  for="fold_acct">Account ID:</label><input style="width: 125px;" id="fold_acct" name="fold_acct" type="text" value="<?php echo $fold_acct;?>" /><? if ($fold_acct == 'fold-id') { echo '<font style="color: #ff0000;">Set your FAH name!</font>'; } ?></p>
+			<p><label for="fold_expy">Refresh (in Hours):</label>
+			<input style="width: 50px;" id="fold_expy" name="fold_expy" type="text" value="<?php echo $fold_expy;?>" /><br />			
+					<label for="fold_acct">Stats Alignment:</label>
+					Left:<input id="fold_align" name="fold_align" type="radio" value="left" <?php if(get_option('folding_align') == 'left') {?> checked="checked" <?php } ?> />
+					Right:<input id="fold_align" name="fold_align" type="radio" value="right" <?php if(get_option('folding_align') == 'right') {?> checked="checked" <?php } ?> /></p>
+					<p><label for="fold_pic">Show Picture:</label>
+					<input id="fold_pic" name="fold_pic" type="checkbox" value="<?php print get_option('folding_pic');?>" <?php if(get_option('folding_pic') == 'true') {?> checked="checked" <?php } ?>/></p>
+					<p><label for="fold_results_bold">Results in Bold?:</label>
+					<input id="fold_results_bold" name="fold_results_bold" type="checkbox" value="<?php print get_option('folding_results_bold');?>" <?php if(get_option('folding_results_bold') == 'bold') {?> checked="checked" <?php } ?>/></p>		
+					<p><label for="fold_results_color">Results Color:</label>
+					<input id="fold_results_color" name="fold_results_color" type="text" value="<?php print get_option('folding_results_color');?>" style="width: 60px;" /> Eg: <font style="color: #ff0000;">ff0000</font><font style="color: #00ff00;"> 00ff00 </font><font style="color: #c0c0c0;">c0c0c0</font></p>
+					<p><label for="fold_rank">Show full rank:</label>
+					<input id="fold_rank" name="fold_rank" type="checkbox" value="<?php print get_option('folding_rank_show');?>" <?php if(get_option('folding_rank_show') == 'full') {?> checked="checked" <?php } ?>/></p>
+			    </fieldset>
+				<?php if ($fold_acct != 'fold-id'){
+				echo '<h2>Preview</h2>';
+				echo '<p>';
+				echo 'Total Score: <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_credit').'</font><br />';
+				echo 'OverallRank: <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_rank').'</font><br />';
+				echo 'WorkUnits  : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_wu').'</font><br />';
+				if (get_option('folding_wut')) {
+				echo 'OtherUnits : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_wut').'</font><br />';
+				}
+				echo 'LastUpdate : <font style="font-weight: '.get_option('folding_results_bold').'; color: #'.get_option('folding_results_color').';">'.get_option('folding_last').'</font>';
+				if (get_option('folding_pic') == 'true') {
+					echo '<br /><a href="http://folding.stanford.edu"><img src="'.get_settings('home') . '/wp-content/plugins/folding-stats-plus/FAHlogoML.jpg'.'" alt="Folding@Home" /></a>';
+					}
+				echo '</p>';
+				}
+
+				?>
+			
+			
+			
+			 <p class="submit">
+			      <input type="submit" name="Submit" value="<?php _e('Update options') ?> &raquo;" /> <input id="fold_del" name="fold_del" type="checkbox" value="delete" /><label for="fold_del"> Reset cache?</label>
+			    </p>
+			  </form>
+</div></div></div>
+
+
+</div>
